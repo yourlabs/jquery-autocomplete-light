cda795c83e2a693ce5c2521fa9a48f7568f52ec5
jpic <jamespic@gmail.com>
2015-05-30 13:04:46 +0200

    Tab to space

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,41 +1,41 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
+    grunt.initConfig({
 
-		// Import package manifest
-		pkg: grunt.file.readJSON("package.json"),
+        // Import package manifest
+        pkg: grunt.file.readJSON("package.json"),
 
-		// Banner definitions
-		meta: {
-			banner: "/*\n" +
-				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
-				" *  <%= pkg.description %>\n" +
-				" *  <%= pkg.homepage %>\n" +
-				" *\n" +
-				" *  Made by <%= pkg.author.name %>\n" +
-				" *  Under <%= pkg.license %> License\n" +
-				" */\n"
-		},
+        // Banner definitions
+        meta: {
+            banner: "/*\n" +
+                " *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
+                " *  <%= pkg.description %>\n" +
+                " *  <%= pkg.homepage %>\n" +
+                " *\n" +
+                " *  Made by <%= pkg.author.name %>\n" +
+                " *  Under <%= pkg.license %> License\n" +
+                " */\n"
+        },
 
-		// Concat definitions
-		concat: {
-			options: {
-				banner: "<%= meta.banner %>"
-			},
-			dist: {
-				src: [
+        // Concat definitions
+        concat: {
+            options: {
+                banner: "<%= meta.banner %>"
+            },
+            dist: {
+                src: [
                     "src/autocomplete.js",
                     "src/addanother.js",
                     "src/remote.js",
                     "src/text_widget.js",
                     "src/widget.js"
                 ],
-				dest: "dist/jquery.autocomplete-light.js"
-			}
-		},
+                dest: "dist/jquery.autocomplete-light.js"
+            }
+        },
 
-		// Lint definitions
-		jshint: {
+        // Lint definitions
+        jshint: {
             files: [
                 "src/autocomplete.js",
                 "src/addanother.js",
@@ -43,40 +43,40 @@ module.exports = function(grunt) {
                 "src/text_widget.js",
                 "src/widget.js"
             ],
-			options: {
-				jshintrc: ".jshintrc"
-			}
-		},
+            options: {
+                jshintrc: ".jshintrc"
+            }
+        },
 
-		// Minify definitions
-		uglify: {
-			my_target: {
-				src: ["dist/jquery.autocomplete-light.js"],
-				dest: "dist/jquery.autocomplete-light.min.js"
-			},
-			options: {
-				banner: "<%= meta.banner %>"
-			}
-		},
+        // Minify definitions
+        uglify: {
+            my_target: {
+                src: ["dist/jquery.autocomplete-light.js"],
+                dest: "dist/jquery.autocomplete-light.min.js"
+            },
+            options: {
+                banner: "<%= meta.banner %>"
+            }
+        },
 
-		// watch for changes to source
-		// Better than calling grunt a million times
-		// (call 'grunt watch')
-		watch: {
-		    files: ['src/*'],
-		    tasks: ['default']
-		}
+        // watch for changes to source
+        // Better than calling grunt a million times
+        // (call 'grunt watch')
+        watch: {
+            files: ['src/*'],
+            tasks: ['default']
+        }
 
-	});
+    });
 
-	grunt.loadNpmTasks("grunt-contrib-concat");
-	grunt.loadNpmTasks("grunt-contrib-jshint");
-	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks("grunt-contrib-coffee");
-	grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-coffee");
+    grunt.loadNpmTasks("grunt-contrib-watch");
 
-	grunt.registerTask("build", ["concat", "uglify"]);
-	grunt.registerTask("default", ["jshint", "build"]);
-	grunt.registerTask("travis", ["default"]);
+    grunt.registerTask("build", ["concat", "uglify"]);
+    grunt.registerTask("default", ["jshint", "build"]);
+    grunt.registerTask("travis", ["default"]);
 
 };
