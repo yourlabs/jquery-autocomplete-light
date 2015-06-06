3dbc0dec4c9c9b63fa9012c704ddb39d9fed1289
jpic <jamespic@gmail.com>
2015-06-06 15:03:30 +0200

    Extracted django admin specific hooks from widget.js

--- src/widget.js
+++ src/widget.js
@@ -365,56 +365,8 @@ $(document).ready(function() {
     //      });
     $(document).trigger('yourlabsWidgetReady');
 
-    $('.autocomplete-light-widget').on('initialize', function() {
-        /*
-        Set .change-related link when the admin loads, because it's apparently
-        unable to do it server side on initial rendering, not sure why, would
-        love to know !
-
-        For django 1.8 admin support.
-        */
-        var value = $(this).find('select').val();
-
-        if (value !== undefined) {
-            var next = $(this).next();
-            var template = next.attr('data-href-template');
-
-            if (template && next.is('.change-related')) {
-                next.attr('href', template.replace('__fk__', value));
-            }
-        }
-    });
-
     $('.autocomplete-light-widget:not([id*="__prefix__"])').each(function() {
         $(this).trigger('initialize');
-    });
-
-    $(document).bind('widgetDeselectChoice', function(e, choice, widget) {
-        /*
-        Unset the .change-related link when an item is selected.
-
-        For django 1.8 admin support.
-        */
-        var next = widget.widget.next();
-        var template = next.attr('data-href-template');
-
-        if (template && next.is('.change-related') && next.attr('href')) {
-            next.removeAttr('href');
-        }
-    });
-
-    $(document).bind('widgetSelectChoice', function(e, choice, widget) {
-        /*
-        Set the .change-related link when an item is selected.
-
-        For django 1.8 admin support.
-        */
-        var next = widget.widget.next();
-        var template = next.attr('data-href-template');
-
-        if (template && next.is('.change-related')) {
-            next.attr('href', template.replace('__fk__', widget.getValue(choice)));
-        }
     });
 
     $(document).bind('DOMNodeInserted', function(e) {
