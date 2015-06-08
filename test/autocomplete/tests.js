217d96e6e23c4f2b183cb75535f9967395d18025
jpic <jamespic@gmail.com>
2015-06-08 22:09:04 +0200

    Tested $.yourlabsRegistry()

--- test/autocomplete/tests.js
+++ test/autocomplete/tests.js
@@ -1,3 +1,14 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+QUnit.test('$.yourlabsRegistry is defined', function(assert) {
+  assert.notOk($.fn.yourlabsRegistry === undefined);
+});
+
+QUnit.test('$.yourlabsRegistry stores and retrieves', function(assert) {
+  var $input = $('input[name=no-id]');
+
+  $input.yourlabsRegistry('foo', 'bar');
+  assert.equal($input.yourlabsRegistry('foo'), 'bar');
+
+  var fixture = {'foo': 'bar'};
+  $input.yourlabsRegistry('test', fixture);
+  assert.equal($input.yourlabsRegistry('test'), fixture);
 });
